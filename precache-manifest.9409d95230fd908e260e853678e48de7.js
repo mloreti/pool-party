a5fe77546de4a22ed93ab9250487c82063a9b3c8
Mickey Loreti <miloreti@microsoft.com>
2019-05-29 15:23:52 -0700

    Updates

--- precache-manifest.9409d95230fd908e260e853678e48de7.js
+++ precache-manifest.9409d95230fd908e260e853678e48de7.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "baf9b529fc506e05d0294dd8dcae8535",
+    "revision": "ac4e4825f1addce03113bec7d3c3b04d",
     "url": "/pool-party/index.html"
   },
   {
-    "revision": "aa06ea4c30faa46b35b5",
-    "url": "/pool-party/static/css/main.38d66ddc.chunk.css"
+    "revision": "ca650cd79a45e4c2e840",
+    "url": "/pool-party/static/css/main.9357edae.chunk.css"
   },
   {
     "revision": "7fac32297ba7d3fb912b",
     "url": "/pool-party/static/js/2.9c51a64a.chunk.js"
   },
   {
-    "revision": "aa06ea4c30faa46b35b5",
+    "revision": "ca650cd79a45e4c2e840",
     "url": "/pool-party/static/js/main.fd50be28.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pool-party/precache-manifest.029278523705aa72c88fe6f914146e6c.js"
+  "/pool-party/precache-manifest.9409d95230fd908e260e853678e48de7.js"
 );
 
 self.addEventListener('message', (event) => {
